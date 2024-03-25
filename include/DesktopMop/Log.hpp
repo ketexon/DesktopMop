5ad00611208cb102e138a600df124664dab707b6
ketexon <zane.a.s.clark@gmail.com>
2024-03-24 17:22:24 -0700

    fixed __FILE__ etc. from being nonwide in logger

--- include/DesktopMop/Log.hpp
+++ include/DesktopMop/Log.hpp
@@ -9,8 +9,12 @@
 #define DESKTOPMOP_LOG_LEVEL_ERROR 1
 #define DESKTOPMOP_LOG_LEVEL_OFF 0
 
+// Taken from https://stackoverflow.com/questions/14421656/is-there-widely-available-wide-character-variant-of-file
+#define DESKTOPMOP_WIDE_IMPL(x) L##x
+#define DESKTOPMOP_WIDE(x) DESKTOPMOP_WIDE_IMPL(x)
+
 #if DESKTOPMOP_DEBUG
-#define _WLOG_TRACE << __FILE__ << L"::" << __FUNCTION__ << L"(" << __LINE__ << L"): "
+#define _WLOG_TRACE << DESKTOPMOP_WIDE(__FILE__) << L"::" << DESKTOPMOP_WIDE(__FUNCTION__) << L"(" << __LINE__ << L"): "
 #else
 #define _WLOG_TRACE
 #endif
